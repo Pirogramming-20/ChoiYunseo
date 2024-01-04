47178411931c53d2bfbe342c6987c54bba11c378
yunseo <chldbstj0219@naver.com>
2024-01-05 02:51:47 +0900

    [1-3]

--- python_problem/python_problem1.py
+++ python_problem/python_problem1.py
@@ -1,2 +1,16 @@
 num = 0
-playNum = input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): ")
+playNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+
+while True:
+    try:
+        playNum = int(playNum)
+        if playNum not in [1,2,3]:
+            print("1,2,3 중 하나를 입력하세요.")
+            playNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+        else:
+            break
+    except ValueError:
+        print("정수를 입력하세요.")
+        playNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+
+playNum = int(playNum)
