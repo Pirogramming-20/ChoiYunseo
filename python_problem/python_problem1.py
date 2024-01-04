26caf1aede3237211a81a10fce8e1e749a845a62
yunseo <chldbstj0219@naver.com>
2024-01-05 03:50:08 +0900

    1-8

--- python_problem/python_problem1.py
+++ python_problem/python_problem1.py
@@ -1,22 +1,26 @@
 num = 0
 
-while True:
-    playNumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
-
+def brgame(a):
     while True:
         try:
-            playNumA = int(playNumA)
-            if playNumA not in [1,2,3]:
+            a = int(playNumA)
+            if a not in [1,2,3]:
                 print("1,2,3 중 하나를 입력하세요.")
-                playNumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+                a = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
             else:
                 break
         except ValueError:
             print("정수를 입력하세요.")
-            playNumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+            a = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
 
-    playNumA = int(playNumA)
+while True:
+    #playerA
+    playNumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
 
+    brgame(playNumA)
+
+    playNumA=int(playNumA)
+    
     for i in range(playNumA):
         callNumA=num+1+i
         if callNumA!=32:
@@ -27,22 +31,12 @@ while True:
 
     num = num+playNumA
 
-    #playB
+    #playerB
     playNumB = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
 
-    while True:
-        try:
-            playNumB = int(playNumB)
-            if playNumB not in [1,2,3]:
-                print("1,2,3 중 하나를 입력하세요.")
-                playNumB = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
-            else:
-                break
-        except ValueError:
-            print("정수를 입력하세요.")
-            playNumB = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+    brgame(playNumB)
 
-    playNumB = int(playNumB)
+    playNumB=int(playNumB)
 
     for i in range(playNumB):
         callNumB=num+1+i
