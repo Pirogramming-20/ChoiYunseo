4f3c9410e86f60218ef03c56abe4ef242d6f832c
yunseo <chldbstj0219@naver.com>
2024-01-05 03:27:35 +0900

    1-6

--- python_problem/python_problem1.py
+++ python_problem/python_problem1.py
@@ -1,19 +1,59 @@
 num = 0
-playNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
-
 while True:
-    try:
-        playNum = int(playNum)
-        if playNum not in [1,2,3]:
-            print("1,2,3 중 하나를 입력하세요.")
-            playNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+    playNumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+
+    while True:
+        try:
+            playNumA = int(playNumA)
+            if playNumA not in [1,2,3]:
+                print("1,2,3 중 하나를 입력하세요.")
+                playNumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+            else:
+                break
+        except ValueError:
+            print("정수를 입력하세요.")
+            playNumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+
+    playNumA = int(playNumA)
+
+    for i in range(playNumA):
+        callNumA=num+1+i
+        if callNumA!=32:
+            print("playerA :",callNumA)
         else:
             break
-    except ValueError:
-        print("정수를 입력하세요.")
-        playNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
 
-playNum = int(playNum)
+    num = num+playNumA
 
-for i in range(playNum):
-    print("playerA :",num+1+i)
+    #playB
+    playNumB = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+
+    while True:
+        try:
+            playNumB = int(playNumB)
+            if playNumB not in [1,2,3]:
+                print("1,2,3 중 하나를 입력하세요.")
+                playNumB = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+            else:
+                break
+        except ValueError:
+            print("정수를 입력하세요.")
+            playNumB = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+
+    playNumB = int(playNumB)
+
+    for i in range(playNumB):
+        callNumB=num+1+i
+        if callNumB!=32:
+            print("playerB :",callNumB)
+        else:
+            break
+
+    num = num+playNumB
+
+
+
+
+
+    
+
