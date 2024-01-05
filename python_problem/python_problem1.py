f4a6f878665c1ddbbd531909d5b8572d86ac7d43
yunseo <chldbstj0219@naver.com>
2024-01-06 02:51:51 +0900

    1-8 수정(최종)

--- python_problem/python_problem1.py
+++ python_problem/python_problem1.py
@@ -1,59 +1,63 @@
 num = 0
-while True:
-    playNumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
 
+def brgame(a):
     while True:
         try:
-            playNumA = int(playNumA)
-            if playNumA not in [1,2,3]:
+            a = int(a)
+            if a not in [1,2,3]:
                 print("1,2,3 중 하나를 입력하세요.")
-                playNumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+                a = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
             else:
-                break
+                return a
         except ValueError:
             print("정수를 입력하세요.")
-            playNumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+            a = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
 
-    playNumA = int(playNumA)
+while True:
+    #playerA
+    playerNumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
 
-    for i in range(playNumA):
-        callNumA=num+1+i
-        if callNumA<31:
-            print("playerA",callNumA)
-        elif callNumA == 31:
-            print("palyerA 31")
+    playerNumA=brgame(playerNumA) #함수 안에서 제대로 다시 받은 값을 playerNum으로 가지도록
+
+    playerNumA=int(playerNumA)
+
+    for i in range(playerNumA):
+        callPlayerNumA=num+1+i
+        if callPlayerNumA<31:
+            print("playerA",callPlayerNumA)
+        elif callPlayerNumA ==31:
+            print("playerA 31")
+            print("playerB win!")
             break
-
-    num = num+playNumA
+        else:
+            print("playerB win!")
+            break
     
-    if callNumA == 31:
+    num = num+playerNumA
+
+    if callPlayerNumA == 31:
         break
 
-    #playB
-    playNumB = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+    #playerB
+    playerNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
 
-    while True:
-        try:
-            playNumB = int(playNumB)
-            if playNumB not in [1,2,3]:
-                print("1,2,3 중 하나를 입력하세요.")
-                playNumB = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
-            else:
-                break
-        except ValueError:
-            print("정수를 입력하세요.")
-            playNumB = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+    playerNum=brgame(playerNum) #함수 안에서 제대로 다시 받은 값을 playerNum으로 가지도록
 
-    playNumB = int(playNumB)
+    playerNum=int(playerNum)
 
-    for i in range(playNumB):
-        callNumB=num+1+i
-        if callNumB<31:
-            print("playerB",callNumB)
-        elif callNumB == 31:
+    for i in range(playerNum):
+        callPlayerNum=num+1+i
+        if callPlayerNum<31:
+            print("playerB",callPlayerNum)
+        elif callPlayerNum ==31:
             print("playerB 31")
+            print("playerA win!")
             break
+        else:
+            print("playerA win!")
+            break
+    
+    num = num+playerNum
 
-    num = num+playNumB
-    if callNumB == 31:
+    if callPlayerNum == 31:
         break
